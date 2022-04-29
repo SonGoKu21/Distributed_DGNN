716ab3e610a2c0b68f3de06d907cf6595cb975ab
Fahao Chen <chenfh612@gmail.com>
2022-04-29 22:03:24 +0900

    update

--- customized_ddp.py
+++ customized_ddp.py
@@ -4,7 +4,7 @@ Supportive modules to conduct distributed training
 import torch
 import torch.nn as nn
 from torch._utils import _flatten_dense_tensors, _unflatten_dense_tensors
-from .utils import get_torch_default_comm
+from utils import get_torch_default_comm
 
 
 class DistributedGroupedDataParallel(nn.Module):
