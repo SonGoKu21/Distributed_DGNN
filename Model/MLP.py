201d7a270b95fd9181b3fed375090a9f196d75fe
Fahao Chen <chenfh612@gmail.com>
2022-04-30 22:06:39 +0900

    update

--- Model/MLP.py
+++ Model/MLP.py
@@ -28,11 +28,11 @@ class Classifier(torch.nn.Module):
 
     def forward(self, x):
         x = self.layer_1(x)
-        # x = self.norm(x)
+        x = self.norm(x)
         x = self.activation3(x)
 
         x = self.layer_2(x)
-        # x = self.norm(x)
+        x = self.norm(x)
         x = self.activation3(x)
 
         x = self.layer_3(x)
