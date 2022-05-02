bd09ab939f778ed61ec3659310be025be9c3858f
Fahao Chen <chenfh612@gmail.com>
2022-05-02 13:54:31 +0900

    update

--- test_function.py
+++ test_function.py
@@ -91,7 +91,7 @@ def run_dgnn_distributed(args):
     model = _My_DGNN(args, in_feats=load_feats[0].shape[1]).to(device)
     model.set_comm()
     # model = LocalDDP(copy.deepcopy(model), mp_group, dp_group, world_size)
-    print('worker {} already put the model to device {}'.format(rank, args['data_str']))
+    print('worker {} already put the model to device {}'.format(rank, args['device']))
     model = DDP(model, process_group=dp_group)
 
     # loss_func = nn.BCELoss()
