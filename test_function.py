8679c45f08834f978f2b1a85e26fd2ea942d4828
Fahao Chen <chenfh612@gmail.com>
2022-05-04 21:59:13 +0900

    update

--- test_function.py
+++ test_function.py
@@ -99,8 +99,8 @@ def run_dgnn_distributed(args):
     else:
         args['temporal_time_steps'] = num_graph
     print("Worer {} loads {}/{} graphs, where {} local graphs, {} remote graphs.".format(
-        rank, num_graph, args['time_steps']),
-        num_graph, args['temporal_time_steps'] -1)
+        rank, num_graph, args['time_steps'],
+        num_graph, args['temporal_time_steps'] -1))
 
     # generate dataset
     dataset = load_dataset(*get_data_example(load_g, args, num_graph))
